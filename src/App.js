01068f1a79d76d99ba88723c65e0a5d045fce2ef
Preeti Jatav <preeti.jatav@Preeti-Jatav-MBP.local>
2022-03-02 17:12:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { ReactDOM } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// ---Function Component---
+// const App = () => {
+//   return (
+//   <select>
+//   <option value="grapefruit">Grapefruit</option>
+//   <option value="lime">Lime</option>
+//   <option selected value="coconut">Coconut</option>
+//   <option value="mango">Mango</option>
+// </select>
+//   );
+// }
+
+// ---Class Component---
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'coconut'};
+
+    // this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // handleChange(event) {
+  //   this.setState({value: event.target.value});
+  // }
+
+  handleSubmit(event) {
+       alert('Your favorite flavor is: ' + this.state.value);
+        event.preventDefault();
+  }
+
+  handleChange = event => {
+    this.setState({value: event.target.value});
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your favorite flavor:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
 export default App;
